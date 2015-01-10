fb5cc0a41c7640ca5f6462bb88bd1e8b62240e79
Emil Bergold <emil.bergold@gmail.com>
2015-01-11 00:49:21 +0100

    bootstrap

---
+++ lib/world.dart
@@ -0,0 +1,11 @@
+library world;
+
+import 'dart:html';
+
+class World {
+
+  final HtmlElement container;
+
+  World(this.container);
+
+}
---
+++ web/main.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2015, Emil Bergold. All rights reserved. Use of this source code
+// is governed by the MIT license that can be found in the LICENSE file.
+
+import 'dart:html';
+
+import 'package:dots/world.dart';
+
+void main() {
+  var elmWorld = querySelector('#world');
+  var world = new World(elmWorld);
+
+}

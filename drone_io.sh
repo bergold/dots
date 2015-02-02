36a9123dc8f8ebb1f21f0125629eabbc2b4b5dcf
Emil <emil.bergold@outlook.com>
2015-02-02 22:06:14 +0100

    Update and rename Makefile to drone_io.sh

---
+++ drone_io.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -o xtrace
+set -e
+
+pub get
+pub build
+
+cd build/web
+git init
+git config user.name "Drone.io"
+git config user.email "emil.bergold@outlook.com"
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --verbose "https://${GITHUB_AUTH}@${DRONE_REPO_SLUG}.git" master:gh-pages

c5f3382d8f3f87ca33d7be52fd24df7dfcd78de8
Emil <emil.bergold@outlook.com>
2015-02-06 20:48:47 +0100

    SSH url

--- drone_io.sh
+++ drone_io.sh
@@ -10,5 +10,4 @@ git config user.name "Drone.io"
 git config user.email "emil.bergold@outlook.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
-ls -al ~/.ssh
-git push --force "https://bergold@${DRONE_REPO_SLUG}.git" master:gh-pages
+git push --force "git@github.com:bergold/dots.git" master:gh-pages

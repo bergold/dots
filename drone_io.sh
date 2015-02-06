070c18ea12889345e1c349b1248e1e1f82501639
Emil <emil.bergold@outlook.com>
2015-02-06 20:44:16 +0100

    Dump ssl certs

--- drone_io.sh
+++ drone_io.sh
@@ -10,4 +10,5 @@ git config user.name "Drone.io"
 git config user.email "emil.bergold@outlook.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force "https://${DRONE_REPO_SLUG}.git" master:gh-pages
+ls -al ~/.ssh
+git push --force "https://bergold@${DRONE_REPO_SLUG}.git" master:gh-pages

161acb41a158f1d75620f245bd342e48506614a9
Emil <emil.bergold@outlook.com>
2015-02-22 10:32:42 +0100

    advanced debugconsole

--- web/scripts/debugconsole.dart
+++ web/scripts/debugconsole.dart
@@ -4,15 +4,26 @@ void initDebugConsole(DotWorld world, Driver driver) {
 
   var container = querySelector('#debugconsole');
   container.attributes.remove('hidden');
+  container.parent.attributes.remove('hidden');
 
-  container.querySelector('#btn1').onClick.listen((e) => driver.start());
-  container.querySelector('#btn2').onClick.listen((e) => driver.stop());
-  container.querySelector('#btn3').onClick.listen((e) => world.drop());
-  container.querySelector('#btn4').onClick.listen((e) => world.drop(20));
-  container.querySelector('#btn5').onClick.listen((e) => world.reset());
-  container.querySelector('#btn6').onClick.listen((e) {
+  // driver
+  container.querySelector('#driver_start').onClick.listen((e) => driver.start());
+  container.querySelector('#driver_stop').onClick.listen((e) => driver.stop());
+  container.querySelector('#driver_reset').onClick.listen((e) => driver.reset());
+  container.querySelector('#driver_1sec').onClick.listen((e) {
     driver.start();
     new Timer(new Duration(seconds: 1), () => driver.stop());
   });
+
+  // world
+  container.querySelector('#world_drop_n').onClick.listen((e) {
+    var inpN = container.querySelector('#world_drop_n_count') as InputElement;
+    var n = inpN.valueAsNumber;
+    world.drop(n);
+  });
+  container.querySelector('#world_drop_custom').onClick.listen((e) {
+
+  });
+  container.querySelector('#world_reset').onClick.listen((e) => world.reset());
 
 }
